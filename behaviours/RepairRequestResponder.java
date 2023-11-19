803edd62106ea0f15c893ed8e1b24170027647eb
EmmanuelADAM <emmanueladam@free.fr>
2023-11-20 00:41:03 +0100

    start behaviour to handle request

---
+++ behaviours/RepairRequestResponder.java
@@ -0,0 +1,19 @@
+package behaviours;
+
+import jade.core.Agent;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import jade.proto.AchieveREResponder;
+
+public class RepairRequestResponder extends AchieveREResponder {
+
+    public RepairRequestResponder(Agent a, MessageTemplate mt) {
+        super(a, mt);
+    }
+
+    @Override
+    public ACLMessage handleRequest(ACLMessage request){
+
+        return null;
+    }
+}
